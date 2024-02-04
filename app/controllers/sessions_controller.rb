7a52b2ea52ced9d692a45818189635bd00f62e80
“MatsumuraSaki” <massaki3156@gmail.com>
2024-02-05 00:19:43 +0900

    ログイン機能実装

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,20 @@
+class SessionsController < ApplicationController
+  def new
+  end
+  
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user&.authenticate(params[:session][:password])
+      # ログイン成功した場合
+      session[:user_id] = user.id
+      redirect_to user_path(user.id)
+    else
+      # ログイン失敗した場合
+      flash.now[:danger] = 'ログインに失敗しました'
+      render :new
+    end
+  end
+
+  def destroy
+  end
+end
