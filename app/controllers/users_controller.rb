a185f5127a9c8ed059e2c23733d686452d4d0ba4
“MatsumuraSaki” <massaki3156@gmail.com>
2024-02-05 00:53:33 +0900

    アクセス制限追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,7 @@
 class UsersController < ApplicationController
+  skip_before_action :login_required, only: [:new, :create]
+  before_action :correct_user, only: [:show]
+
   def new
     @user = User.new
   end
@@ -22,5 +25,10 @@ class UsersController < ApplicationController
   private
   def user_params
     params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
+
+  def correct_user
+    @user = User.find(params[:id])
+    redirect_to current_user unless current_user?(@user)
   end
 end
