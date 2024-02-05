645c0238347b1232a7aae7a09fec4820fb38e4bc
“MatsumuraSaki” <massaki3156@gmail.com>
2024-02-05 13:31:12 +0900

    タスク一覧には自分で作ったタスクしか表示しないようにする機能実装

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -2,15 +2,15 @@ class TasksController < ApplicationController
   before_action :set_task, only: %i[ show edit update destroy ]
 
   def index
-    @tasks = Task.all
+    @tasks = Task.user_tasks(current_user.id)
   end
 
   def new
-    @task = Task.new
+    @task = current_user.tasks.build
   end
 
   def create
-    @task = Task.new(task_params)
+    @task = current_user.tasks.build(task_params)
     if @task.save
       redirect_to tasks_path, notice: t('.created')
     else
--- app/models/task.rb
+++ app/models/task.rb
@@ -1,4 +1,9 @@
 class Task < ApplicationRecord
+  belongs_to :user
+
   validates :title, presence: true
   validates :content, presence: true
+
+  scope :user_tasks, -> (query){ where(" tasks.user_id = ? ", query) }
+
 end
