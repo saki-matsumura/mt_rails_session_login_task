af28b8d57af5cea84ecf68be1c082261872702ce
“MatsumuraSaki” <massaki3156@gmail.com>
2024-02-05 21:45:36 +0900

    修正対応

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -37,7 +37,7 @@ class UsersController < ApplicationController
 
   def destroy
     @user.destroy
-    redirect_to new_sessions_path
+    redirect_to new_session_path
   end  
 
   private
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,10 @@
 class User < ApplicationRecord
-  has_many :tasks
+  has_many :tasks, dependent: :destroy
 
   validates :name, presence: true, length: { maximum: 30 }
   validates :email, presence: true, length: { maximum: 255 }, uniqueness: true, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
   before_validation { email.downcase! }
   has_secure_password
+  validates :password, presence: true, on: :update
   validates :password, length: { minimum: 6 }
 end
